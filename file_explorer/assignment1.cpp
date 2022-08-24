7fac10d90cc405de248daae6e58061745eb83e51
bhanujggandhi <gandhibhanuj@gmail.com>
2022-08-24 22:37:20 +0530

    create file

--- file_explorer/assignment1.cpp
+++ file_explorer/assignment1.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
+#include <fcntl.h>
 
 using namespace std;
+
+void create_file()
+{
+    int filediscriptor;
+    string filename;
+
+    cin >> filename;
+
+    filediscriptor = creat(filename.c_str(), S_IRWXU | S_IRGRP | S_IROTH);
+
+    if (filediscriptor == -1)
+        cout << "Failed to create the file" << endl;
+    else
+        cout << "File created" << endl;
+}
 
 int main()
 {
